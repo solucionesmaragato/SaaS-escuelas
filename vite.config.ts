25bedf7563efc2282b87ca349b71b6fe91c6c86e
solucionesmaragato <bokaya22@gmail.com>
2026-06-15 21:45:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import defineConfig from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: true, // 👈 ¡AÑADE ESTA LÍNEA AQUÍ PARA FORZAR EL MOTOR!
+  nitro: true,
   tanstackStart: {
     server: { entry: "server" },
   },
